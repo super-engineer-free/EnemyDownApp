36b986611cef7e85b072833e392172de1c318144
super.engineer <super.engineer.free@gmail.com>
2024-05-26 21:20:57 +0900

    設定値、難易度、各種情報取得処理の作成を完璧にしたい。

---
+++ src/main/java/plugin/enemydownApp/mapper/PlayerScoreMapper.java
@@ -0,0 +1,14 @@
+package plugin.enemydownApp.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import plugin.enemydownApp.mapper.data.PlayerScore;
+
+@Mapper
+public interface PlayerScoreMapper {
+
+
+@Select("select * from player_score order by id asc")
+  List<PlayerScore> selectPlayerScoreList();
+}
